c244e02ee61f6981332e5fefecd55f56d160aded
Soline Villermois <villermois.soline@gmail.com>
2020-10-08 15:41:55 +0200

    test husky

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,44 +1,45 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Button from '@material-ui/core/Button';
 
-import { ButtonStyled } from "./style";
+import { ButtonStyled } from './style';
 
-const Home = ({home, getHome, getHomeWithoutSaga}) => {
-    console.log(home);
-    React.useEffect(() => {
-        getHome()
-    },[getHome]);
+const Home = ({ home, getHome, getHomeWithoutSaga }) => {
+  React.useEffect(() => {
+    getHome();
+  }, [getHome]);
 
-    const [checked, setChecked] = React.useState(false);
+  const [checked, setChecked] = React.useState(false);
 
-    return (
-        <>
-        <div>
-            {
-                home.map(el => <FormControlLabel
-                    key={`check${el}`}
-                    control={<Checkbox checked={checked} />}
-                    label={el}
-                />)
-            }
-        </div>
-            <Button variant="contained" onClick={() => setChecked(!checked)}>{checked ? 'Uncheck it !' : 'Check it !'}</Button>
-            <ButtonStyled onClick={() => getHomeWithoutSaga()}> Add a checkbox </ButtonStyled>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        {home.map((el) => (
+          <FormControlLabel
+            key={`check${el}`}
+            control={<Checkbox checked={checked} />}
+            label={el}
+          />
+        ))}
+      </div>
+      <Button variant="contained" onClick={() => setChecked(!checked)}>
+        {checked ? 'Uncheck it !' : 'Check it !'}
+      </Button>
+      <ButtonStyled onClick={() => getHomeWithoutSaga()}> Add a checkbox </ButtonStyled>
+    </>
+  );
+};
 
 Home.defaultProps = {
-    home: [],
-}
+  home: [],
+};
 
 Home.propTypes = {
-    getHome: PropTypes.func.isRequired,
-    getHomeWithoutSaga: PropTypes.func.isRequired,
-    home: PropTypes.arrayOf(PropTypes.shape),
-}
+  getHome: PropTypes.func.isRequired,
+  getHomeWithoutSaga: PropTypes.func.isRequired,
+  home: PropTypes.arrayOf(PropTypes.shape),
+};
 
 export default Home;
